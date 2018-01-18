4c1d5aaf3f4beb85474b7688317aa639d3d61272
alvin <alvinhtml@gmail.com>
2018-01-18 19:54:25 +0800

    2018-01-18

--- routes/web.php
+++ routes/web.php
@@ -17,55 +17,48 @@
 Route::get('/', 'HomeController@index');
 Route::get('/{urls}', 'HomeController@index')->where('urls','(?!api/).+');
 
-Route::group(['middleware' => 'web', 'prefix' => 'api'], function () {
+Route::get('api/test', 'testController@index');
 
-    Route::get("test", 'testController@index');
+//admins register
+Route::get('api/admin/register', 'Admin\AuthController@showRegistrationForm')->name('admin.register');
+Route::post('api/admin/register', 'Admin\AuthController@register');
 
-    //admins register
-    Route::get('admin/register', 'Admin\AuthController@showRegistrationForm')->name('admin.register');
-    Route::post('admin/register', 'Admin\AuthController@register');
+//admins login
+Route::get('api/admin/login', 'Admin\AuthController@showLoginForm')->name('admin.login');
+Route::post('api/admin/login', 'Admin\AuthController@login');
 
-    //admins login
-    Route::get('admin/login', 'Admin\AuthController@showLoginForm')->name('admin.login');
-	Route::post('admin/login', 'Admin\AuthController@login');
-
-    //admins logout
-	Route::get('admin/logout', 'Admin\AuthController@logout')->name("admin.logout");
+//admins logout
+Route::get('api/admin/logout', 'Admin\AuthController@logout')->name("admin.logout");
 
 
-    //Admins list
-    Route::get('admin', 'Admin\AdminController@showAdminList');
-    Route::get('admin/list', 'Admin\AdminController@showAdminList');
-    Route::post('admin/edit/{id?}', 'Admin\AdminController@add');
-    Route::get('admin/view/{id}', 'Admin\AdminController@view');
-    Route::get('admin/del/{id}', 'Admin\AdminController@del');
-    Route::get('admin/edit_state/{id}', 'Admin\AdminController@edit_state');
+//Admins list
+Route::get('api/admin', 'Admin\AdminController@showAdminList');
+Route::get('api/admin/list', 'Admin\AdminController@showAdminList');
+Route::post('api/admin/edit/{id?}', 'Admin\AdminController@add');
+Route::get('api/admin/view/{id}', 'Admin\AdminController@view');
+Route::get('api/admin/del/{id}', 'Admin\AdminController@del');
+Route::get('api/admin/edit_state/{id}', 'Admin\AdminController@edit_state');
 
-    //term list
-    Route::get('term', 'TermController@showTermList');
-    Route::get('term/list', 'TermController@showTermList');
-    Route::post('term/edit/{id?}', 'TermController@add');
-    Route::get('term/view/{id}', 'TermController@view');
-    Route::get('term/del/{id}', 'TermController@del');
-    // Route::get('term/edit_state/{id}', 'TermController@edit_state');
+//term list
+Route::get('api/term', 'TermController@showTermList');
+Route::get('api/term/list', 'TermController@showTermList');
+Route::post('api/term/edit/{id?}', 'TermController@add');
+Route::get('api/term/view/{id}', 'TermController@view');
+Route::get('api/term/del/{id}', 'TermController@del');
+// Route::get('api/term/edit_state/{id}', 'TermController@edit_state');
 
-    //ou list
-    Route::get('ou', 'OuController@showOuList');
-    Route::get('ou/list', 'OuController@showOuList');
-    Route::post('ou/edit/{id?}', 'OuController@add');
-    Route::get('ou/view/{id}', 'OuController@view');
-    Route::get('ou/del/{id}', 'OuController@del');
-    Route::get('ou/component', 'OuController@componentlist');
-    // Route::get('ou/edit_state/{id}', 'OuController@edit_state');
+//ou list
+Route::get('api/ou', 'OuController@showOuList');
+Route::get('api/ou/list', 'OuController@showOuList');
+Route::post('api/ou/edit/{id?}', 'OuController@add');
+Route::get('api/ou/view/{id}', 'OuController@view');
+Route::get('api/ou/del/{id}', 'OuController@del');
+Route::get('api/ou/component', 'OuController@componentlist');
+// Route::get('api/ou/edit_state/{id}', 'OuController@edit_state');
 
-    //logined info
-    Route::get('authinfo', 'Admin\AdminController@authInfo')->name("authinfo");
-    //Route::auth();
+//logined info
+Route::get('api/authinfo', 'Admin\AdminController@authInfo')->name("authinfo");
+//Route::auth();
 
-
-    Route::group(['prefix' => 'setting'], function () {
-        Route::post('list_configs', 'SettingController@list_configs');
-    });
-
-
-});
+//setting
+Route::post('api/setting/list_configs', 'SettingController@list_configs');
